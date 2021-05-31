6899861d6ae50eb57d55bd01f07bc949cfad213a
emlahieu98 <hieucr98tb@gmail.com>
2021-05-31 16:43:32 +0700

    add paginate

--- backend/services/users/src/modules/user/user.controller.ts
+++ backend/services/users/src/modules/user/user.controller.ts
@@ -1,6 +1,6 @@
 import { updateUserDTO } from '../../models/user.dto';
 import { UserEntity } from 'src/entities/user.entity';
-import { Body, Controller, Get, UseGuards, Put, ValidationPipe } from '@nestjs/common';
+import { Body, Controller, Get, UseGuards, Put, ValidationPipe, Req, Query } from '@nestjs/common';
 import { AuthGuard } from '@nestjs/passport';
 import { UserService } from './user.service';
 import { User } from 'src/decorator/user.decorator';
@@ -8,28 +8,45 @@ import { Roles } from 'src/decorator/roles.decorator';
 import { RolesGuard } from 'src/guards/roles.guard';
 import { ApiBearerAuth, ApiUnauthorizedResponse } from '@nestjs/swagger';
 
-
+import {Request} from "express";
 
 @Controller('users')
 @ApiBearerAuth('access-token')
 export class UserController {
-
   constructor(
     private userService: UserService
   ) { }
-
+  
 
   @Get()
   @Roles('admin')
-  
   @UseGuards(AuthGuard(), RolesGuard)
   
   
   //@ApiUnauthorizedResponse()
-  all() {
-    return this.userService.find()
-  }
-
+  // async all(@Req() req: Request) {
+  //   const builder = await this.userService.queryBuilder('users');  
+  //       if (req.query.s) {
+  //           builder.where("users.email LIKE :s OR users.username LIKE :s", {q: `%${req.query.q}%`})
+  //       }
+  //       const sort: any = req.query.sort;
+  //       if (sort) {
+  //           builder.orderBy('users.created', sort.toUpperCase());
+  //       }
+  //       const page: number = parseInt(req.query.page as any) || 1;
+  //       const perPage = 9;
+  //       const total = await builder.getCount();
+  //       builder.offset((page - 1) * perPage).limit(perPage);
+  //       return {
+  //           data: await builder.getMany(),
+  //           total,
+  //           page,
+  //           last_page: Math.ceil(total / perPage)
+  //       };
+  // }
+  async all(@Query() query) {
+    return this.userService.find(query)
+    }
   @Put()
   @UseGuards(AuthGuard())
   @ApiBearerAuth()
--- backend/services/users/src/modules/user/user.service.ts
+++ backend/services/users/src/modules/user/user.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, Req } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { UserEntity } from 'src/entities/user.entity';
 import { Repository } from 'typeorm';
@@ -10,9 +10,29 @@ export class UserService {
      @InjectRepository(UserEntity) private userRepo: Repository<UserEntity>,
   )    
   { }
-  async find() {
-    return this.userRepo.find()
+  async find(query) {
+    const {email, username, page , sort} = query
+    const builder = this.userRepo.createQueryBuilder('users')
+    if (email) {   
+            await builder.where("users.email LIKE :email", {email: `%${email}%`})
+    }
+    if (username) {   
+            await builder.where("users.username LIKE :username", {username: `%${username}%`})
+    }
+    if (sort) {
+        builder.orderBy('users.created', sort.toUpperCase());
+    }
+        const perPage = 5;
+        const total = await builder.getCount();
+    builder.offset((page - 1) * perPage).limit(perPage);
+        return {
+            data: await builder.getMany(),
+            total,
+            page,
+            last_page: Math.ceil(total / perPage)
+        };
   }
+  
   async findByEmail(email: string): Promise<UserEntity>{
     return this.userRepo.findOne({where: {email}})
   }
